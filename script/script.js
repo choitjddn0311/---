a5bd248ad0d1a4375281fcc043fc7cd125402573
choitjddn0311 <cswcjw311427@naver.com>
2024-01-10 12:56:43 +0900

    feat:goods_area_carousel

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $(".menu > ul > li").mouseover(function(){
-        $(".sub_menu").slideDown();
-    }).mouseover(function(){
-        $(".sub_menu").slideUp();
+    $(".shop").mouseover(function(){
+        $(".sub_menu").stop().slideDown();
+    }).mouseout(function(){
+        $(".sub_menu").stop().slideUp();
     });
 });
 
