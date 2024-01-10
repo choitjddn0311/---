2e64a0464e6f5847c9c587be383bf4f12e89369d
choitjddn0311 <cswcjw311427@naver.com>
2024-01-10 17:03:21 +0900

    test :script carousel teest

--- script/script.js
+++ script/script.js
@@ -4,5 +4,12 @@ $(document).ready(function(){
     }).mouseout(function(){
         $(".sub_menu").stop().slideUp();
     });
+
+    // $("버튼").click(function(){
+    //     ~~다음 창이보이기 (car1~car3) 
+    // });
+    // $("뒤로가기버튼").click(function(){
+    //     ~그 전창을 보내기 (ex> car2 --> car 1, car3--> car1 , car1 --> car3)
+    // })
 });
 
